d26ecdc1fad40b6ebcfae16a695a4cf40b6474f8
Andrew Waller <andrewhwaller@gmail.com>
2019-03-17 00:24:12 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -3,7 +3,6 @@
 def loop_iterator(number_of_times)
   loop number_of_times.do
     puts phrase = "Welcome to Flatiron School's Web Development Course!"
-    end
 end
 
 def times_iterator(number_of_times)
