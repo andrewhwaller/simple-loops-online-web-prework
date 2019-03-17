fc8f578b462b4d9abc00a6f16c017b9e6bb8fc6d
Andrew Waller <andrewhwaller@gmail.com>
2019-03-17 00:27:49 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,8 +1,13 @@
 # REMEMBER: print your output to the terminal using 'puts'
 
 def loop_iterator(number_of_times)
-  loop number_of_times.times do
-    puts phrase = "Welcome to Flatiron School's Web Development Course!"
+  counter = 0
+  phrase = "Welcome to Flatiron School's Web Development Course!"
+  loop do
+    puts phrase
+    counter += 1
+    break if counter == number_of_times
+  end
 end
 
 def times_iterator(number_of_times)
